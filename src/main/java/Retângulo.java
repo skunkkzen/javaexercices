baf997d9137930cde3dca575213df58a8ec0e205
Victor <victordestefani@hotmail.com>
2019-11-13 19:24:42 +0000

    Retângulo

--- src/main/java/Retângulo.java
+++ src/main/java/Retângulo.java
@@ -1,2 +1,36 @@
+// VARIAVEIS
+
 public class Retângulo {
+
+    private float length = 1.0f;
+    private float width = 1.0f;
+
+//CONSTRUTORES
+
+    public Retângulo() {
+
+    }
+
+    public Retângulo(float length) {
+        this.length = length;
+    }
+
+    public Retângulo(float length, float width) {
+        this.length = length;
+        this.width = width;
+    }
+
+
+//MÉTODOS
+public float getLength () {return length;}
+public float getWidth() {return width;}
+
+public void setLength(float length)  {this.length = length;}
+public void setWidth(float width) {this.width = width;}
+public double getArea() {return Math.PI * length * length;}
+public double getPerimeter() {return Math.PI * length * length;}
+
+    public String toString() {
+        return String.format("Retângulo: Length: 1.0f, Color= 1.0f, Area: 0.2f", length, width, getArea()); }
+
 }
